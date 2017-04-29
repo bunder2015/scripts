d70c8339deb834d42bd0286e358263f2b30876ee
bunder2015 <omfgbunder@gmail.com>
2017-04-29 01:18:51 -0400

    Added rollback/regression testing to zfs buildbot

--- zfs/testing/buildbot.sh
+++ zfs/testing/buildbot.sh
@@ -12,6 +12,9 @@ ZFS_CHECKOUT="master"
 ###
 # THESE ARE OPTIONAL
 ###
+#SPL_ROLLBACK_COMMIT=""
+#ZFS_ROLLBACK_COMMIT=""
+#
 #SPL_TEMP_PATCHES=""
 #ZFS_TEMP_PATCHES="https://patch-diff-github/raw/user/repo/pull/1234.patch
 #https://github/user/repo/commit/a5s6d7f8.patch"
@@ -25,6 +28,9 @@ git clone $ZFS_REPO
 
 cd /home/testuser/spl
 git checkout $SPL_CHECKOUT
+if [ -n "$SPL_ROLLBACK_COMMIT" ] ; then
+	git reset --hard $SPL_ROLLBACK_COMMIT
+fi
 if [ -n "$SPL_TEMP_PATCHES" ] ; then
 	for f in $SPL_TEMP_PATCHES ; do
 		curl -s $f | git am
@@ -36,6 +42,9 @@ make
 
 cd /home/testuser/zfs
 git checkout $ZFS_CHECKOUT
+if [ -n "$ZFS_ROLLBACK_COMMIT" ] ; then
+	git reset --hard $ZFS_ROLLBACK_COMMIT
+fi
 if [ -n "$ZFS_TEMP_PATCHES" ] ; then
 	for f in $ZFS_TEMP_PATCHES ; do
 		curl -s $f | git am
