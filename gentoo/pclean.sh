0bfb8134365615e9bf65691b2e02826870f0a565
bunder2015 <omfgbunder@gmail.com>
2017-06-05 01:31:36 -0400

    pclean now uses directory style package.file/file

--- gentoo/pclean.sh
+++ gentoo/pclean.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-
-
-if [ -z $1 ] ; then
+if [ -z "$1" ] ; then
 	echo "Help: pclean.sh (env|keywords|unmask|use)"
 	exit 1
-elif [ $1 = "env" ] ; then
-	FILE="package.env"
-elif [ $1 = "keywords" ] ; then
-	FILE="package.keywords"
-elif [ $1 = "unmask" ] ; then
-	FILE="package.unmask"
-elif [ $1 = "use" ] ; then
-	FILE="package.use"
+elif [ "$1" = "env" ] ; then
+	DIR="/etc/portage/package.env"
+	FILE="env"
+elif [ "$1" = "keywords" ] ; then
+	DIR="/etc/portage/package.keywords"
+	FILE="keywords"
+elif [ "$1" = "unmask" ] ; then
+	DIR="/etc/portage/package.unmask"
+	FILE="unmask"
+elif [ "$1" = "use" ] ; then
+	DIR="/etc/portage/package.use"
+	FILE="use"
 else
 	echo "Help: pclean.sh (env|keywords|unmask|use)"
 	exit 1
 fi
 
-while read i ; do
-        [[ $i = \#* || -z $i ]] && continue
-	str=$(echo $i | awk '{print $1}')
-	equery -q list $str &>/dev/null
-	if [ $? -ne 0 ] ; then
-		echo "$str can be removed from $FILE"
-        fi
-done < /etc/portage/"$FILE"
-
+if [ -f $DIR/$FILE ] ; then
+	while read -r i ; do
+		[[ $i = \#* || -z $i ]] && continue
+		str=$(echo "$i" | awk '{print $1}')
+		if ! equery -q list "$str" &>/dev/null ; then
+			echo "$str can be removed from $DIR/$FILE"
+		fi
+	done < $DIR/$FILE
+else
+	echo "$DIR/$FILE does not exist"
+fi
