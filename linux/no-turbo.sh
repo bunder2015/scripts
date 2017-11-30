ff0974bf7a39774d23bf7a5cc87f39d39f20efa3
bunder2015 <omfgbunder@gmail.com>
2017-11-30 17:08:19 -0500

    Updated no-turbo script

--- linux/no-turbo.sh
+++ linux/no-turbo.sh
@@ -3,6 +3,8 @@
 DEBUG=0
 
 # This turns off Intel turbo boost because the BIOS won't let us do it in there
+# Requirements: CONFIG_X86_INTEL_PSTATE
+
 until [ -e /sys/devices/system/cpu/intel_pstate/no_turbo ]; do
 	if [ "$DEBUG" -eq "1" ]; then
 		printf "Waiting for no_turbo to appear...\n"
@@ -12,17 +14,23 @@ done
 
 OUTPUT=`cat /sys/devices/system/cpu/intel_pstate/no_turbo`
 
-until [ "$OUTPUT" -eq "1" ]; do
-	echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
-	OUTPUT=`cat /sys/devices/system/cpu/intel_pstate/no_turbo`
-	if [ "$OUTPUT" -ne "1" ]; then
-		if [ "$DEBUG" -eq "1" ]; then
-		printf "Turbo did not get disabled, trying again...\n"
+if [ "$OUTPUT" -ne "1" ]; then
+	until [ "$OUTPUT" -eq "1" ]; do
+		echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+		OUTPUT=`cat /sys/devices/system/cpu/intel_pstate/no_turbo`
+		if [ "$OUTPUT" -ne "1" ]; then
+			if [ "$DEBUG" -eq "1" ]; then
+			printf "Turbo did not get disabled, trying again...\n"
+			fi
+			sleep 5
+		else
+			if [ "$DEBUG" -eq "1" ]; then
+				printf "Turbo should now be disabled...\n"
+			fi
 		fi
-		sleep 5
-	else
-		if [ "$DEBUG" -eq "1" ]; then
-			printf "Turbo should now be disabled...\n"
-		fi
+	done
+else
+	if [ "$DEBUG" -eq "1" ]; then
+	        printf "Turbo is already disabled.\n"
 	fi
-done
+fi
